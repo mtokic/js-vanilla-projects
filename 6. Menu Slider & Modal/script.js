86c062018cbfe9285afffac47ede7edf77373af9
Mario Tokić <mtokic@Marios-MBP.home>
2021-06-21 17:07:35 +0200

    new line - selector

--- 6. Menu Slider & Modal/script.js
+++ 6. Menu Slider & Modal/script.js
@@ -1,5 +1,6 @@
 const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
+
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 
